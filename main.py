695a5cc2f7786d48841c1b8e7fccef1f6d659147
w0terlife <zane.tashakor11@hotmail.com>
2025-07-09 16:05:29 -0700

    Created Main.py
    added 1.0 attributes

---
+++ main.py
@@ -0,0 +1,22 @@
+name = input('ribbit - need name')
+print("wussup", name)
+
+day = input('do u have a goog day')
+
+if day == 'yes' or day == 'Yes': 
+  print('ok good. I LIKE FROGS 𓆏')
+elif day == 'no' or day == 'No':
+  print('dang it, I LIKE FROGS 𓆏')
+else:
+  print('𓆏FROG SAYS HE DOES NOT UNDERSTAND')
+
+a = input('do you want to hear a frog joke?𓆏')
+
+if a == 'yes' or day == 'Yes': 
+  first_joke = input('What did the frog order at FrogDonald’s?')
+  if first_joke == 'french flies and a diet croak':
+    print('HHHOOOOOWWWWW!!!!! Froggie no like u anymore 𓆏')
+  else:
+    print('french flies and a diet croak 🤣🤣🤣')
+else:
+  print(' Froggie no like u anymore 𓆏')
